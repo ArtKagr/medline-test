0038dd452fd9a3203b8f17daca8ca34f7ea942fa
kagr <artemiokrasz@yandex.ru>
2020-07-12 22:12:30 +0200

    add pluralize pages

--- src/components/PostsCounter/PostsCounter.vue
+++ src/components/PostsCounter/PostsCounter.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="posts_counter">
-        В нашем блоге уже {{postsCounter}} статей
+        В нашем блоге уже {{postsCounter}} {{pluralizePosts(postsCounter)}}
     </div>
 </template>
 
 <script>
+    import { pluralizeWords } from "@/tools/tools";
     export default {
         name: 'posts-counter',
         computed: {
             postsCounter() {
                 return this.$store.getters['posts/postsCounter']
             }
+        },
+        methods: {
+            pluralizePosts(hour) {
+                return pluralizeWords(hour, ['статья', 'статьи', 'статей']);
+            },
         }
     }
 </script>
